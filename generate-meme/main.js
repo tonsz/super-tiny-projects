f5c5758f5349fd6d371dd4efa40ad4b552f7e265
tonsz <tmestrera@gmail.com>
2023-04-28 11:57:37 +0800

    First JS practice

---
+++ generate-meme/main.js
@@ -0,0 +1,20 @@
+const generateMemeBtn = document.querySelector(".meme-generator .meme-generator-btn")
+const memeImage = document.querySelector(".meme-generator img")
+const memeTitle = document.querySelector(".meme-generator h2")
+const memeAuthor = document.querySelector(".meme-generator .meme-author")
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url)
+    memeTitle.innerHTML = title
+    memeAuthor.innerHTML = `Meme by: ${author}`
+}
+
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((res) => res.json())
+    .then(data => {
+        updateDetails(data.url, data.title, data.author)
+    })
+}
+
+generateMemeBtn.addEventListener("click", generateMeme)
