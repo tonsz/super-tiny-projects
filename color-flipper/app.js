7a272fc501c82ba8ce81d6dece2fb56d41853353
tonsz <tmestrera@gmail.com>
2023-05-06 12:21:12 +0800

    Color wallet feature

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,26 +1,46 @@
 const container = document.querySelector(".container")
 const flipperButton = document.getElementById("color-flipper")
-const hexCode = document.getElementById("hex-code")
+const saverButton = document.getElementById("color-saver")
+const inventory = document.querySelector(".inventory")
+const hexCodeDisplay = document.getElementById("hex-code")
+const resetButton = document.getElementById("background-reset")
 
-const hexArr = '0123456789ABCDEF'
-let code = '#';
+const hexDigits = '0123456789ABCDEF'
+let colorCollection = []
+let hexCode = '#';
+let hexCodeBuffer = '';
 
-const handleClick = () => {
+const generateCode = () => {
 
-    // generate a random hex number 
     for(let i = 0; i < 6; i++) {
-        code += hexArr[Math.floor(Math.random() * 16)]
+        hexCode += hexDigits[generateRandomNum()]
     }
+
+    hexCodeDisplay.innerText = hexCode
+    container.style.backgroundColor = hexCode
     
-    // put that hex number as the innerhtml of hexCode
-    hexCode.innerText = code
-
-    // change the container's background color to that color 
-    container.style.backgroundColor = code
-
     // reset
-    code = '#';
- 
+    hexCodeBuffer = hexCode
+    hexCode = '#'
+    
 }
 
-flipperButton.addEventListener("click", handleClick)
+const saveColor = () => {
+    const newColor = document.createElement('div')
+    newColor.classList.add('square')
+    inventory.appendChild(newColor)
+    newColor.style.backgroundColor = hexCodeBuffer
+    
+}
+
+const resetBackground = () => {
+    hexCodeDisplay.innerText = "#FFFFFF"
+    container.style.backgroundColor = "#FFFFFF";
+}
+const generateRandomNum = () => {
+    return Math.floor(Math.random() * 16)
+}
+
+flipperButton.addEventListener("click", generateCode)
+saverButton.addEventListener("click", saveColor)
+resetButton.addEventListener("click", resetBackground)
