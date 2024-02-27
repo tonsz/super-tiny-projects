b1338e491cbb30d30ca1cb885f536f2d1f9c6df4
tonsz <tmestrera@gmail.com>
2024-02-27 13:58:33 +0800

    changed to document ready

--- bored_api/script.js
+++ bored_api/script.js
@@ -1,4 +1,4 @@
-jQuery(function () {
+$(document).ready(function () {
   $("#btn").click(function () {
     $.get("http://www.boredapi.com/api/activity", function (response) {
       console.log(response);
