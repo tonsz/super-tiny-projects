d307148a76dd6638ae59873821f27fb052fdc722
tonsz <tmestrera@gmail.com>
2024-02-28 10:03:40 +0800

    added back the s

--- bored_api/script.js
+++ bored_api/script.js
@@ -1,6 +1,6 @@
 jQuery(function () {
   $("#btn").click(function () {
-    $.get("http://www.boredapi.com/api/activity", function (response) {
+    $.get("https://www.boredapi.com/api/activity", function (response) {
       console.log(response);
       document.getElementById("activity").innerHTML = response.activity;
     });
