c3eb58f7e58ca1603db170a8d9fd3df8af9f6365
tonsz <tmestrera@gmail.com>
2024-02-28 09:37:49 +0800

    try network

--- bored_api/script.js
+++ bored_api/script.js
@@ -1,6 +1,6 @@
 jQuery(function () {
   $("#btn").click(function () {
-    $.get("https://www.boredapi.com/api/activity", function (response) {
+    $.get("http://www.boredapi.com/api/activity", function (response) {
       console.log(response);
       document.getElementById("activity").innerHTML = response.activity;
     });
